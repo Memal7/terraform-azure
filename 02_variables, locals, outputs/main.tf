4340fea54705152286724153f68d8450acb4fd97
Mustafa Emal <mustafaemal@microsoft.com>
2022-05-29 18:14:26 +0200

    storage account dependencies edited

--- 02_variables, locals, outputs/main.tf
+++ 02_variables, locals, outputs/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "rg" {
 # create a storage account
 resource "azurerm_storage_account" "storage" {
   name                     = local.storage_name
-  resource_group_name      = var.resource_group
+  resource_group_name      = azurerm_resource_group.rg
   location                 = var.location
   account_tier             = var.storage_tier
   account_replication_type = var.storage_replication_type
