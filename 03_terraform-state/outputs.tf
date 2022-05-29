9064e4b1bc8780bdd027c061674ae48f56ae55fd
Mustafa Emal <mustafaemal@microsoft.com>
2022-05-29 16:54:24 +0200

    terraform import added

--- 03_terraform-state/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Create an output for resource group id
-output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
-}
-
-# create an output for the storage account name
-output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
-}
