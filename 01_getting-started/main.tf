953f7def96ce08ae98c2f59cb08cabc16c5fbd13
Mustafa Emal <mustafa.emal@microsoft.com>
2022-12-01 13:12:19 +0100

    storage renamed

--- 01_getting-started/main.tf
+++ 01_getting-started/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # create a storage account
-resource "azurerm_storage_account" "test" {
+resource "azurerm_storage_account" "stg" {
   name                     = "stgaccacctest001"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
